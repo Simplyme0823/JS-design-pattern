75b325d169d737016a714a744eb320fb1f0e1dae
Simplyme0823 <1213966903@qq.com>
2020-04-21 23:49:12 +0800

    书本中的发布订阅模式详细注释，完成度80%…

---
+++ test.js
@@ -0,0 +1,34 @@
+/*
+Object.apply
+Object.arguments
+Object.assign
+Object.bind
+Object.call
+Object.caller
+Object.create
+Object.defineProperties
+Object.defineProperty
+Object.freeze
+Object.getOwnPropertyDescriptor
+Object.getOwnPropertyNames
+Object.getOwnPropertySymbols
+Object.getPrototypeOf
+Object.is
+Object.isExtensible
+Object.isFrozen
+Object.isSealed
+Object.keys
+Object.length
+Object.name
+Object.preventExtensions
+Object.prototype
+Object.seal
+Object.setPrototypeOf
+Object.toString
+*/
+
+let a =0.1
+let b = 0.2
+if(a + b == 0.3){
+    console.log('0.3')
+}
